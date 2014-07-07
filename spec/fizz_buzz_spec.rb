c0da48f594f745338e580e471d5ddb1873a6c14b
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 08:05:34 -0700

    run second test for numbers divisible by 5, fail test

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('fizz_buzz')
 
 describe('fizz_buzz')do
- it('list numbers up to a value, replace numbers divisible by 3 with the word Fizz')do
+ it('lists numbers up to a value, replaces numbers divisible by 3 with the word Fizz')do
   expect(fizz_buzz(3)).to eq([1, 2, 'Fizz'])
+ end
+ it('replaces numbers divisible by 5 with the word Buzz') do
+  expect(fizz_buzz(5)).to eq([1, 2, 'Fizz', 4, 'Buzz'])
  end
 end
