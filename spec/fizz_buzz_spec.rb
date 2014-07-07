96481d92fc6b48f4b331124a23ae9c0d138fee02
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 07:11:48 -0700

    first test red

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('fizz_buzz')
+
+describe('fizz_buzz')do
+ it('list numbers up to a value, replace numbers divisible by 3 with the word Fizz')do
+  expect(fizz_buzz(3)).to eq('1, 2, Fizz')
+ end
+end
