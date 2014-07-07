97f700b3005a48793922cbbd70e7680aa7895c4e
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 08:03:12 -0700

    pass test with function

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,6 +1,13 @@
 def fizz_buzz(limit)
-  fb_array = [1, 2, "Fizz"]
-  fb_array
+  fizz_buzz_series = Array.new
+  number = 1
+  limit.times do
+    if number % 3 == 0
+     fizz_buzz_series.push('Fizz')
+    else fizz_buzz_series.push(number)
+    end
+    number = number + 1
+  end
+  fizz_buzz_series
 end
-
 
