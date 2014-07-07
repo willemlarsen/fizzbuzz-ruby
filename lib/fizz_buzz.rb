120b68cfb0c3039661129c5b7286da8ffbac7fba
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 08:06:53 -0700

    pass test for numbers divisible by 5 --> Buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,6 +4,8 @@ def fizz_buzz(limit)
   limit.times do
     if number % 3 == 0
      fizz_buzz_series.push('Fizz')
+    elsif number % 5 == 0
+      fizz_buzz_series.push('Buzz')
     else fizz_buzz_series.push(number)
     end
     number = number + 1
