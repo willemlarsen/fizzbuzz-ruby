5d266de73f929c4d673ef7d8b9f811e841cdc8eb
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 08:12:21 -0700

    pass test for FizzBuzz, numbers divisible by 3 and 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,7 +2,9 @@ def fizz_buzz(limit)
   fizz_buzz_series = Array.new
   number = 1
   limit.times do
-    if number % 3 == 0
+    if (number % 3 == 0 && number % 5 == 0)
+      fizz_buzz_series.push('FizzBuzz')
+    elsif number % 3 == 0
      fizz_buzz_series.push('Fizz')
     elsif number % 5 == 0
       fizz_buzz_series.push('Buzz')
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -8,4 +8,7 @@ describe('fizz_buzz')do
  it('replaces numbers divisible by 5 with the word Buzz') do
   expect(fizz_buzz(5)).to eq([1, 2, 'Fizz', 4, 'Buzz'])
  end
+ it('replaces numbers divisible by 3 *and* 5 with the words FizzBuzz') do
+  expect(fizz_buzz(15)).to eq([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
+ end
 end
